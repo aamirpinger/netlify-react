33c8c58a6a7d87d23d7cf41d2411b94ebba0ece9
Aamir Pinger <muhammad.pinger@trilogy.com>
2020-10-04 22:38:10 +0500

    updated the text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world - React App</h1>
+      <h1>Hello world - React App - updated</h1>
     </div>
   );
 }
